564b7baec4565d2417581c8f7eb777911bde475e
AriSendic <rabbisendic@gmail.com>
2016-08-16 09:14:30 -0500

    fixed empty task validation

--- app/assets/javascripts/todo_ctrl.js
+++ app/assets/javascripts/todo_ctrl.js
@@ -4,20 +4,36 @@
 
   angular.module("app").controller("todoCtrl", function($scope) {
     $scope.list = [
-      "Get Haircut",
-      "Build Rome",
-      "Take out trash"
+      {
+        text: "Get Haircut",
+        completed: false
+      },
+      {
+        text: "Build Rome",
+        completed: false
+      },
+      {
+        text: "Take out trash",
+        completed: false
+      }
     ];
-    $scope.addItem = function(inputItem) {
-      if (inputItem !== '') {
-        $scope.list.push(inputItem);
+    $scope.addItem = function(inputText) {
+      if (inputText) {
+        var newTask = {
+          text: inputText,
+          completed: false
+        };
+        $scope.list.push(newTask);
         $scope.newItem = "";
       }
-    // window.$scope = $scope;
+      window.$scope = $scope;
     // debugging tool that lets you view in the console
     };
-    $scope.completeTask = function(inputIndex) {
-      $scope.list.splice(inputIndex, 1);
+    // $scope.completeTask = function(inputIndex) {
+    //   $scope.list.splice(inputIndex, 1);
+    // };
+    $scope.toggleCompleted = function(task) {
+      task.completed = !task.completed;
     };
   });
 
